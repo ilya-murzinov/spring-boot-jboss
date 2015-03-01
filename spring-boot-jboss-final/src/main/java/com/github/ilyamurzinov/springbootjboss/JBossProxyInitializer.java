4de45c0964f3284487560de3d8798b3427842e56
Ilya Murzinov <murz42@gmail.com>
2015-03-01 22:31:39 +0300

    dealing with servlet

--- spring-boot-jboss-final/src/main/java/com/github/ilyamurzinov/springbootjboss/JBossProxyInitializer.java
+++ spring-boot-jboss-final/src/main/java/com/github/ilyamurzinov/springbootjboss/JBossProxyInitializer.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.Filter;
-import javax.servlet.ServletException;
+import javax.servlet.*;
+import java.io.IOException;
 import java.util.Collections;
 
 /**
@@ -35,12 +37,49 @@ public class JBossProxyInitializer implements BeanFactoryPostProcessor {
         filterRegistrationBean.setUrlPatterns(Collections.singletonList("/*"));
 
         beanFactory.registerSingleton("helloFilterProxyFilterRegistrationBean", filterRegistrationBean);
+
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(new DispatcherServletProxy());
+        servletRegistrationBean.setUrlMappings(Collections.singletonList("/*"));
+
+        beanFactory.registerSingleton("dispatcherServletProxyRegistrationBean", servletRegistrationBean);
     }
 
     public static class HelloFilterProxy extends DelegatingFilterProxy {
         @Override
         protected Filter initDelegate(WebApplicationContext wac) throws ServletException {
             return Application.webApplicationContext.getBean(HelloFilter.class);
+        }
+    }
+
+    public static class DispatcherServletProxy implements Servlet {
+
+        private Servlet delegate;
+
+        @Override
+        public void init(ServletConfig config) throws ServletException {
+            delegate = Application.webApplicationContext.getBean(DispatcherServlet.class);
+            delegate.init(config);
+        }
+
+        @Override
+        public ServletConfig getServletConfig() {
+            return delegate.getServletConfig();
+        }
+
+        @Override
+        public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+            delegate.service(req, res);
+        }
+
+        @Override
+        public String getServletInfo() {
+            return delegate.getServletInfo();
+        }
+
+        @Override
+        public void destroy() {
+            delegate.destroy();
         }
     }
 }
